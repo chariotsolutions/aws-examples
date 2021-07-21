fa9930b080a5bf10cb7b46bc2830f4e5a6340e6d
Evan Morton <emorton@chariotsolutions.com>
2021-07-21 16:38:16 -0400

    Another PR fix

--- two_buckets_and_a_lambda/terraform/variables.tf
+++ two_buckets_and_a_lambda/terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "base_lambda_name" {
     description = "The base for names of lambdas"
     type        = string
-    default     = "two_bucks_and_lambda"
+    default     = "two_buckets_and_a_lambda"
 }
 
 variable "api_gateway_name" {
