cc28716ae00fb47acf172c8cb6c619b3d80ab8e2
Keith Gregory <51966316+kgregory-chariot@users.noreply.github.com>
2023-04-26 14:32:37 -0400

    cloudtrail_to_elasticsearch: various updates (#13)
    * write batches based on size rather than record count
    * flatten "serviceEventDetails" sub-object
    * cloudformation: change names, instance type, storage type

--- cloudtrail_to_elasticsearch/cloudtrail_to_elasticsearch/es_helper.py
+++ cloudtrail_to_elasticsearch/cloudtrail_to_elasticsearch/es_helper.py
@@ -21,6 +21,8 @@ import os
 
 from aws_requests_auth.aws_auth import AWSRequestsAuth
 
+DEFAULT_BATCH_SIZE = 7680 * 1024
+
 
 class ESHelper:
 
@@ -40,7 +42,7 @@ class ESHelper:
         mock instance.
     """
 
-    def __init__(self, hostname=None, use_aws_auth=True, use_https=True, index_config=None, batch_size=500):
+    def __init__(self, hostname=None, use_aws_auth=True, use_https=True, index_config=None, batch_size=DEFAULT_BATCH_SIZE):
         """
             hostname      If provided, the hostname of the Elasticsearch cluster. If not
                           provided, this is read from the environment variable ES_HOSTNAME.
@@ -48,6 +50,9 @@ class ESHelper:
             use_https     If true, uses HTTPS requests; if false, HTTP.
             index_config  If provided, used to create a new index. This is a Python object
                           that corresponds to the Elasticsearch configuration JSON.
+            batch_size    if provided, defines a trigger for writing batches to Elasticsearch.
+                          AWS Opensearch has been observed to reject requests > 1 MB, even
+                          though the Elasticsearch docs say that it will accept up to 100 MB.
         """
         if hostname:
             self.hostname = hostname
@@ -70,9 +75,10 @@ class ESHelper:
         else:
             self.protocol = "http"
         self.index_config = index_config
-        self.batch_size = batch_size
+        self.max_batch_size = batch_size
         self.current_index = None
-        self.batch = []
+        self.current_batch = []
+        self.current_batch_size = 0
 
 
     def add_events(self, events, index):
@@ -83,9 +89,19 @@ class ESHelper:
         if self.current_index != index:
             self.flush()  # this is a no-op first time through
             self.current_index = index
-        self.batch += events
-        if len(self.batch) > self.batch_size:
-            self.flush()
+        for event in events:
+            prepared = self.prepare_event(event, self.current_index)
+            self.current_batch.append(prepared)
+            self.current_batch_size += len(prepared)
+            if self.current_batch_size > self.max_batch_size:
+                self.flush()
+
+
+    def prepare_event(self, event, index):
+        return "\n".join([
+            json.dumps({ "index": { "_index": index, "_id": event['eventID'] }}),
+            json.dumps(event)
+            ]) + "\n"
 
 
     def flush(self):
@@ -93,17 +109,16 @@ class ESHelper:
             the batch.
             """
         # no-op to simplify calling code
-        if not self.current_index or not self.batch:
+        if not self.current_index or not self.current_batch:
             return
-        print(f'writing {len(self.batch)} events to index {self.current_index}')
         self.ensure_index_exists(self.current_index)
-        updates = [self.prepare_event(event, self.current_index) for event in self.batch]
-        rsp = self.do_request(requests.post, "_bulk", "".join(updates), 'application/x-ndjson')
+        print(f'writing {len(self.current_batch)} events to index {self.current_index}')
+        rsp = self.do_request(requests.post, "_bulk", "".join(self.current_batch), 'application/x-ndjson')
         if rsp.status_code == 200:
             # individual records may be rejected -- we'll assume them damaged and drop
             self.log_record_errors(rsp)
-            self.current_index = None
-            self.batch = []
+            self.current_batch = []
+            self.current_batch_size = 0
         elif rsp.status_code == 429:
             print(f'upload throttled; retrying')
             # we'll hope that it succeeds before we blow stack
@@ -122,22 +137,15 @@ class ESHelper:
         if rsp.status_code == 200:
             return
         elif rsp.status_code == 404:
-            print(f"creating index: {index}")
-            if self.index_config:
+            if self.index_config:    
+                print(f"creating index: {index}")
                 rsp = self.do_request(requests.put, index, self.index_config)
+                if rsp.status_code != 200:
+                    raise Exception(f'failed to create index: {rsp.text}')
             else:
               pass  # first PUT will create index
-            if rsp.status_code != 200:
-                raise Exception(f'failed to create index: {rsp.text}')
         else:
             print(f'failed to retrieve index status: {rsp.text}')
-
-
-    def prepare_event(self, event, index):
-        return "\n".join([
-            json.dumps({ "index": { "_index": index, "_id": event['eventID'] }}),
-            json.dumps(event)
-            ]) + "\n"
 
 
     def do_request(self, fn, path, body=None, content_type='application/json'):
